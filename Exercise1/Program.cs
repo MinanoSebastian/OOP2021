1eafb2b0d798f60971db90348730e3ed6dcd395a
セバスティアン ミナノ <32069@ojs.ac.jp>
2021-05-28 16:00:12 +0900

    P79 問題

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -7,6 +7,22 @@ using System.Threading.Tasks;
 namespace Exercise1 {
     class Program {
         static void Main(string[] args) {
+            // 2.1.3
+            var songs = new Song[] {
+                new Song("Let it be","The Beatles",243),
+                new Song("Bones","Galantis",206),
+                new Song("Body to Body","MikePerry",180)
+
+            };
+            PrintSongs(songs);
+        
+        }
+
+        //2.1.4
+        private static void PrintSongs(Song[] songs) {
+            foreach(var song in songs) {
+                Console.WriteLine("{0},{1},{2:m\\:ss}",song.Title,song.ArtistName,TimeSpan.FromSeconds(song.Length));
+            }
         }
     }
 }
