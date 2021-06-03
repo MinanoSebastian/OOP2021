15e3c84163aeabaab48d026617c15ca885abe73d
セバスティアン ミナノ <32069@ojs.ac.jp>
2021-06-03 15:24:34 +0900

    P96

---
+++ section01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace section01 {
+    class Program {
+        static void Main(string[] args) {
+
+            // var names = new List<string> {
+            //     "Tokyo","New Delhi","Bangkok","London","Paris","Berlin","Canberra","Hong Kong",
+            // };
+
+            // IEnumerable<string> query = names.Where(s => s.Length <= 5.Select(s =>s.ToLower());
+            // foreach (string s in query) {
+            //     Console.WriteLine(s);
+            // }
+
+            string[] names = {
+                "Tokyo","New Delhi","Bangkok","London","Paris","Berlin","Canberra" };
+            var query = names.Where(s => s.Length <= 5).ToList();
+
+            query.ForEach(Console.WriteLine);
+
+            foreach (var item in query)
+                Console.WriteLine(item);
+            Console.WriteLine("-------");
+
+            names[0] = "Osaka";
+
+            foreach (var item in query)
+                Console.WriteLine(item);
+
+
+            }
+        }
+    }
+}
