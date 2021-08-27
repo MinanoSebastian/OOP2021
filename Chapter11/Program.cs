f892277f56e04437586645bdf26fd11198c35abd
セバスティアン ミナノ <32069@ojs.ac.jp>
2021-08-27 14:18:28 +0900

    リスト11-9完成

---
+++ Chapter11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section01
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            var novelists = ReadNovelist();
+           
+            foreach(var novelist in novelists)
+            {       
+                Console.WriteLine("{0} ({1}-{2}) - {3}",novelist.Name,novelist.Birth.Year,
+                                novelist.Death.Year,string.Join(",",novelist.Masterpieces));
+
+            }
+        }
+
+        // カスタムクラスのオブジェクトとして要素を取り出す
+        public IEnumerable<Novelist> ReadNovelists()
+        { 
+           var xdoc = XDocument.Load("novelists.xml");
+          
+            var novelists = xdoc.Root.Elements()
+                            .Select(x => new Novelist
+                            {
+                                Name = (string)x.Element("name").
+                                KanaName = (string)(x.Element("name").Attribute("kana")),
+                                Birth = (DateTime)x.Element("birth"),
+                                Death = (DateTime)x.Element("death")
+                                MasterPieces = x.Element("masterpieces")
+                                                .Element("title")
+                                                .Select(title => title.Value)
+                                                .ToArray()
+                            });
+                    return novelists.ToArray();
+
+         }   
+    }
+}
