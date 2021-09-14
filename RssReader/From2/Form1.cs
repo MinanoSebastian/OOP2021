d42022e401d2082e3a794d065bbaa98804ac0c3b
セバスティアン ミナノ <32069@ojs.ac.jp>
2021-09-14 15:59:54 +0900

    途中

--- RssReader/From2/Form1.cs
+++ RssReader/From2/Form1.cs
@@ -12,9 +12,32 @@ namespace From2
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form2(string url)
         {
             InitializeComponent();
+            this.url = url;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            wbBrowser.Url = new Uri(url);
+        }
+
+        // ページの読込が完了すると呼ばれる
+        private void wbBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            btBack.Enabled = wbBrowser.CanGoBack;
+            btForward.Enabled = wbBrowser.CanGoForward;
+        }
+
+        private void btForward_Click(object sender, EventArgs e)
+        {
+            wbBrowser.GoForward();
+        }
+
+        private void btBack_Click(object sender,EventArgs e)
+        {
+            wbBrowser.GoBack();
         }
     }
 }
