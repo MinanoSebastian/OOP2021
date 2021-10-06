8ad398bb60f958dd57800b09723fc7984365e273
太田情報商科専門学校 情報システムコース <ojsinf0sys2019@outlook.jp>
2021-10-06 15:59:27 +0900

    途中経過

--- SendMail/Form1.Designer.cs
+++ SendMail/Form1.Designer.cs
@@ -184,6 +184,7 @@ namespace SendMail
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
