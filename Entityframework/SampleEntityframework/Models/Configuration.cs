451325f5dd61b3da917419d63d9326b15191ba9d
太田情報商科専門学校 情報システムコース <ojsinf0sys2019@outlook.jp>
2021-10-20 16:00:14 +0900

    13途中

---
+++ Entityframework/SampleEntityframework/Models/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEntityframework.Models
+{
+        /* 自動マイグレーション用 *********/
+
+        public class Configration : DbMigrationsConfiguration<BooksDbContext>
+        {
+            public Configration()
+            {
+                AutomaticMigrationsEnabled = true;
+                AutomaticMigrationDataLossAllowed = true;
+                ContextKey = "SampleEntityframework.Models.BooksDbContext";
+            }
+    }
+}
